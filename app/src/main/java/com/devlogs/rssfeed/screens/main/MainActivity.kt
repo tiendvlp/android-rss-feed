e3f28be27ebf2544ca2aef81d069b09cab69df37
tiendvlp <tiendvlp@gmail.com>
2021-11-27 19:53:56 +0700

    add more resource

---
+++ app/src/main/java/com/devlogs/rssfeed/screens/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.devlogs.rssfeed.screens.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devlogs.rssfeed.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_main)
+    }
+}
